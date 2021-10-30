4627e3d05049e534ecd54f07c1de893b20709426
Ahmed Medra <ahmed.medrah@gmail.com>
2021-10-30 19:44:38 +0200

    Update project.py

--- project.py
+++ project.py
@@ -10,15 +10,19 @@ import numpy as np
 df = pd.read_csv('wph.csv')
 df = df.drop(index = df[df.wage_per_hour == 44.5].index)
 df.female = df.female.map({0:'male',1:'female'})
+df['union'] = df['union'].map({0:'not',1:'union'})
 df = df.rename(columns={'female':'gender'})
+a = []
+for i in df[['manufacturing','construction']].values:
+    if i[0] == 1:
+        a.append('manufacturing')
+    elif i[1] == 1:
+        a.append('construction')
+    else:
+        a.append('other')
+df['field'] = a
 
-# Predictions
-default_values = {
-    'union' : 0,
-    'gender' : 0,
-    'manufacturing': 0,
-    'construction': 0
-}
+
 
 # Graph
 # education_wage = px.scatter(
@@ -43,22 +47,21 @@ app.layout = html.Div(children=[
         dcc.Dropdown(
             id='gender_dd',
             options=[{'label':c, 'value':c} for c in df.gender.unique()],
-            style={'width':'200px', 'display':'inline-block','margin':'10px'}
+            style={'width':'200px', 'display':'inline-block','margin':'10px'},
+            placeholder='gender',
+
         ),
         dcc.Dropdown(
-            id='construction_dd',
-            options=[{'label':c,'value':c} for c in df.construction.unique()],
-            style={'width':'200px', 'display':'inline-block','margin':'10px'}
+            id='field_dd',
+            options=[{'label':c,'value':c} for c in df.field.unique()],
+            style={'width':'200px', 'display':'inline-block','margin':'10px'},
+            placeholder='field of work',
         ),
         dcc.Dropdown(
             id='union_dd',
             options=[{'label':c,'value':c} for c in df.union.unique()],
-            style={'width':'200px', 'display':'inline-block','margin':'10px'}
-        ),
-        dcc.Dropdown(
-            id='manufacturing_dd',
-            options=[{'label':c,'value':c} for c in df.manufacturing.unique()],
-            style={'width':'200px', 'display':'inline-block','margin':'10px'}
+            style={'width':'200px', 'display':'inline-block','margin':'10px'},
+            placeholder='union',
         )
     ]),    
     html.Div(children=[
@@ -74,17 +77,15 @@ app.layout = html.Div(children=[
 # Useful placeholders and functions to reduce typing
 callback_inputs =  [
     Input(component_id='gender_dd', component_property='value'),
-    Input(component_id='construction_dd', component_property='value'),
-    Input(component_id='union_dd', component_property='value'),
-    Input(component_id='manufacturing_dd', component_property='value')]
+    Input(component_id='field_dd', component_property='value'),
+    Input(component_id='union_dd', component_property='value')]
 
-def return_df_copy(gender, construction, union, manufacturing):
+def return_df_copy(gender, field, union):
     df_copy = df.copy(deep=True)
     df_copy['mask'] = (
         (df_copy['gender'] == gender) & 
-        (df_copy['construction'] == construction) &
-        (df_copy['union'] == union) &
-        (df_copy['manufacturing'] == manufacturing))
+        (df_copy['field'] == field) &
+        (df_copy['union'] == union))
     df_copy['color'] = np.where(df_copy['mask'] == True, 'blue', 'red')
     return df_copy
 
@@ -93,17 +94,18 @@ def return_df_copy(gender, construction, union, manufacturing):
     Output(component_id='education_plot', component_property='figure'),
     callback_inputs)
 
-def update_edu_plot(gender, construction, union, manufacturing):
-    df_copy = return_df_copy(gender, construction, union, manufacturing)
-    education_wage = px.scatter(data_frame=df_copy, x='education_yrs', y='wage_per_hour', title='Education vs Wage per hour', color=df_copy.color.values.tolist()) 
+def update_edu_plot(gender, field, union):
+    df_copy = return_df_copy(gender, field, union)
+    education_wage = px.density_heatmap(data_frame=df_copy, x='education_yrs', y='wage_per_hour', \
+        title='Education vs Wage per hour', marginal_x='histogram', marginal_y='histogram') 
     return education_wage
 
 @app.callback(
     Output(component_id='age_plot', component_property='figure'),
     callback_inputs)
 
-def update_age_plot(gender, construction, union, manufacturing):
-    df_copy = return_df_copy(gender, construction, union, manufacturing)
+def update_age_plot(gender, field, union):
+    df_copy = return_df_copy(gender, field, union)
     age_wage = px.scatter(data_frame=df_copy, x='age', y='wage_per_hour', title='Age vs Wage per hour', color=df_copy.color.values.tolist())
     return age_wage
 
@@ -112,24 +114,22 @@ def update_age_plot(gender, construction, union, manufacturing):
         callback_inputs
 )
 
-def update_exp_plot(gender, construction, union, manufacturing):
-    df_copy = return_df_copy(gender, construction, union, manufacturing)
+def update_exp_plot(gender, field, union):
+    df_copy = return_df_copy(gender, field, union)
     exp_wage = px.scatter(data_frame=df_copy, x='experience_yrs', y='wage_per_hour', title='Experience years vs Wage per hour', color=df_copy.color.values.tolist())
     return exp_wage
 
 @app.callback(
         Output(component_id='gender_box', component_property='figure'),
-        Input(component_id='construction_dd', component_property='value'),
-        Input(component_id='union_dd', component_property='value'),
-        Input(component_id='manufacturing_dd', component_property='value')
+        Input(component_id='field_dd', component_property='value'),
+        Input(component_id='union_dd', component_property='value')
 )
 
-def update_gender_box(construction, union, manufacturing):
+def update_gender_box(field, union):
     df_copy = df.copy(deep=True)
     df_copy['mask'] = (
-        (df_copy['construction'] == construction) &
-        (df_copy['union'] == union) &
-        (df_copy['manufacturing'] == manufacturing))
+        (df_copy['field'] == field) &
+        (df_copy['union'] == union))
     gender_box = px.box(data_frame=df_copy[df_copy['mask'] == True], x='gender', y='wage_per_hour', title='Gender vs Wage per hour',
             color_discrete_map={'male':'#3295a8','female':'#cf6975'}, color='gender')
 
