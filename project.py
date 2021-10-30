3584318b9be03d044fd5f7523a1407c1b90f01dd
Omar Safwat <omarksafwat@gmail.com>
2021-10-30 18:12:41 +0200

    Moved rund server function to the end of the code

--- project.py
+++ project.py
@@ -71,9 +71,6 @@ app.layout = html.Div(children=[
     ])
 ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
 # Useful placeholders and functions to reduce typing
 callback_inputs =  [
     Input(component_id='gender_dd', component_property='value'),
@@ -97,7 +94,7 @@ def return_df_copy(gender, construction, union, manufacturing):
     callback_inputs)
 
 def update_edu_plot(gender, construction, union, manufacturing):
-    df_copy = return_df_copy()
+    df_copy = return_df_copy(gender, construction, union, manufacturing)
     education_wage = px.scatter(data_frame=df_copy, x='education_yrs', y='wage_per_hour', title='Education vs Wage per hour', color=df_copy.color.values.tolist()) 
     return education_wage
 
@@ -106,7 +103,7 @@ def update_edu_plot(gender, construction, union, manufacturing):
     callback_inputs)
 
 def update_age_plot(gender, construction, union, manufacturing):
-    df_copy = return_df_copy()
+    df_copy = return_df_copy(gender, construction, union, manufacturing)
     age_wage = px.scatter(data_frame=df_copy, x='age', y='wage_per_hour', title='Age vs Wage per hour', color=df_copy.color.values.tolist())
     return age_wage
 
@@ -116,7 +113,7 @@ def update_age_plot(gender, construction, union, manufacturing):
 )
 
 def update_exp_plot(gender, construction, union, manufacturing):
-    df_copy = return_df_copy()
+    df_copy = return_df_copy(gender, construction, union, manufacturing)
     exp_wage = px.scatter(data_frame=df_copy, x='experience_yrs', y='wage_per_hour', title='Experience years vs Wage per hour', color=df_copy.color.values.tolist())
     return exp_wage
 
@@ -126,8 +123,11 @@ def update_exp_plot(gender, construction, union, manufacturing):
 )
 
 def update_gender_box(gender, construction, union, manufacturing):
-    df_copy = return_df_copy()
+    df_copy = return_df_copy(gender, construction, union, manufacturing)
     gender_box = px.box(data_frame=df_copy[df_copy['mask'] == True], x='gender', y='wage_per_hour', title='Gender vs Wage per hour',
             color_discrete_map={'male':'#3295a8','female':'#cf6975'}, color='gender')
 
     return  gender_box
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
